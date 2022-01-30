bcbe2510933bd25e38894b3eb8adb453db4d50c8
Tammy <tammytran@Tammys-MacBook-Pro.local>
2022-01-30 17:59:12 -0600

    implement add and get value

--- src/heapstore/src/page.rs
+++ src/heapstore/src/page.rs
@@ -1,8 +1,10 @@
 use common::ids::{PageId, SlotId};
 use common::PAGE_SIZE;
 use std::convert::TryInto;
-use std::mem;
+use std::mem::{size_of, size_of_val};
+use std::cmp::{max};
 
+pub type ValAddr = u16;
 
 /// The struct for a page. Note this can hold more elements/meta data when created,
 /// but it must be able to be packed/serialized/marshalled into the data array of size
@@ -11,21 +13,89 @@ use std::mem;
 /// up to 8+3*6=26 bytes, leaving the rest (PAGE_SIZE-26 for data) when serialized.
 /// You do not need reclaim header information for a value inserted (eg 6 bytes per value ever inserted)
 /// The rest must filled as much as possible to hold values.
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Slot {
+    start: ValAddr,
+    slot_id: SlotId,
+    end: ValAddr
+}
+pub struct Header {
+    page_id: PageId,
+    //num_rec: u16,
+    slot_arr: Vec<Slot>,
+}
+
 pub(crate) struct Page {
     /// The data for data
+    header: Header,
     data: [u8; PAGE_SIZE],
 }
+
 
 /// The functions required for page
 impl Page {
     /// Create a new page
     pub fn new(page_id: PageId) -> Self {
-        panic!("TODO milestone pg");
+        let new_header = Header {
+            page_id: page_id,
+            //num_rec: 0,
+            slot_arr: Vec::new(),
+        };
+        return Page{header: new_header, data: [0; PAGE_SIZE]};
     }
 
     /// Return the page id for a page
     pub fn get_page_id(&self) -> PageId {
-        panic!("TODO milestone pg");
+        self.header.page_id
+    }
+
+    pub fn get_slot_size(&self, slot: &Slot) -> usize {
+        usize::from(slot.end - slot.start)
+    }
+
+
+    pub fn get_first_free_space(&self, data_size: usize) -> Option<ValAddr> {
+        let num_slot = self.header.slot_arr.len();
+        if num_slot == 0 {
+            return Some((PAGE_SIZE - self.get_header_size()).try_into().unwrap());
+        }
+        let mut total_size: usize = 0;
+        let mut res = None;
+        let mut slot_arr = self.header.slot_arr.clone();
+        slot_arr.sort_unstable();
+        for i in 0..(num_slot - 1) {
+            let slot_size = self.get_slot_size(&slot_arr[i]);
+            total_size += slot_size;
+            let diff = slot_arr[i+1].start - slot_arr[i].end;
+            if diff > 0 && usize::from(diff) >= data_size {
+                res = Some(slot_arr[i].end);
+            }
+        }
+        total_size += self.get_slot_size(&slot_arr[num_slot-1]);
+        if (PAGE_SIZE - self.get_header_size() - total_size) < (data_size + size_of_val(&slot_arr[0])) {
+            return None;
+        }
+        else {
+            match res {
+                None => return Some(slot_arr[0].start - u16::try_from(data_size).unwrap()),
+                Some(x) => return Some(x),
+            }           
+        }
+    }
+
+    pub fn generate_slot_id(&self) -> SlotId {
+        let mut slot_arr = self.header.slot_arr.clone();
+        slot_arr.sort_unstable_by(|a, b| a.slot_id.cmp(&b.slot_id));
+        let mut slot_id = 0;
+        for single_slot in slot_arr {
+            if single_slot.slot_id <= slot_id {
+                slot_id += 1;
+            }
+            else {
+                return slot_id;
+            }
+        }
+        return slot_id;
     }
 
 
@@ -39,14 +109,29 @@ impl Page {
     /// HINT: You can copy/clone bytes into a slice using the following function.
     /// They must have the same size.
     /// self.data[X..y].clone_from_slice(&bytes);
-
     pub fn add_value(&mut self, bytes: &[u8]) -> Option<SlotId> {
-        panic!("TODO milestone pg");
+        let data_size = bytes.len();
+        let first_free_space = self.get_first_free_space(data_size);
+        let mut start_offset: ValAddr = 0;
+        match first_free_space {
+            None => return None,
+            Some(val_addr) => start_offset = val_addr,
+        }
+        let new_slot_id = self.generate_slot_id();
+        let end_offset = start_offset + u16::try_from(data_size).unwrap();
+        self.header.slot_arr.push(Slot {start: start_offset, slot_id: new_slot_id, end: end_offset});
+        self.data[start_offset.try_into().unwrap()..end_offset.try_into().unwrap()].clone_from_slice(&bytes);
+        return Some(new_slot_id);
     }
 
     /// Return the bytes for the slotId. If the slotId is not valid then return None
     pub fn get_value(&self, slot_id: SlotId) -> Option<Vec<u8>> {
-        panic!("TODO milestone pg");
+        for single_slot in &self.header.slot_arr {
+            if single_slot.slot_id == slot_id {
+                return Some(self.data[single_slot.start.try_into().unwrap()..single_slot.end.try_into().unwrap()].to_vec());
+            }
+        }
+        return None;
     }
 
     /// Delete the bytes/slot for the slotId. If the slotId is not valid then return None
@@ -78,15 +163,26 @@ impl Page {
     /// Will be used by tests. Optional for you to use in your code
     #[allow(dead_code)]
     pub(crate) fn get_header_size(&self) -> usize {
-        panic!("TODO milestone pg");
+        //size_of_val(&self.header.page_id) + size_of_val(&self.header.slot_arr)
+        size_of_val(&self.header)
     }
 
     /// A utility function to determine the largest block of free space in the page.
     /// Will be used by tests. Optional for you to use in your code
     #[allow(dead_code)]
     pub(crate) fn get_largest_free_contiguous_space(&self) -> usize {
-        panic!("TODO milestone pg");
-    }
+        let num_slot = self.header.slot_arr.len();
+        if num_slot == 0 {
+            return PAGE_SIZE - self.get_header_size();
+        }
+        let mut slot_arr = self.header.slot_arr.clone();
+        let mut res = PAGE_SIZE - self.get_header_size() - usize::from(slot_arr[0].start);
+        slot_arr.sort_unstable();
+        for i in 0..(num_slot-1) {
+            res = max(res, (slot_arr[i+1].start - slot_arr[i].end).into());
+        }
+        return res;     
+    }   
 }
 
 /// The (consuming) iterator struct for a page.
