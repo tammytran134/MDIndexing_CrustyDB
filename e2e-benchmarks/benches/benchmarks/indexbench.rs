51b28a0104cb25cb088af9ad868951059e6a68b5
Tammy <tammytran@Tammys-MacBook-Pro.local>
2022-05-20 14:49:47 -0500

    e

--- e2e-benchmarks/benches/benchmarks/indexbench.rs
+++ e2e-benchmarks/benches/benchmarks/indexbench.rs
@@ -62,44 +62,6 @@ fn setup_data_range(bt: &mut Template, dim: usize) -> Vec<String> {
     res
 }
 
-fn setup_data_range(bt: &mut Template, dim: usize) -> Vec<Vec<String>> {
-    let mut res = Vec::new();
-    let mut rng = rand::thread_rng();
-    if dim == 2 {
-        for i in 0..data_num {
-            let mut sub_res = Vec::new();
-            let num1 = rng.gen_range(0..MAX);
-            let num2 = rng.gen_range(num1+1..MAX);
-            let num3 = rng.gen_range(0..MAX);
-            let num4 = rng.gen_range(num3+1..MAX);
-            sub_res.push(num1.to_string());
-            sub_res.push(num2.to_string());
-            sub_res.push(num3.to_string());
-            sub_res.push(num4.to_string());
-            res.push(sub_res.clone());
-        }
-    }
-    else {
-        for i in 0..data_num {
-            let mut sub_res = Vec::new();
-            let num1 = rng.gen_range(0..MAX);
-            let num2 = rng.gen_range(num1+1..MAX);
-            let num3 = rng.gen_range(0..MAX);
-            let num4 = rng.gen_range(num3+1..MAX);
-            let num5 = rng.gen_range(0..MAX);
-            let num6 = rng.gen_range(num5+1..MAX);
-            sub_res.push(num1.to_string());
-            sub_res.push(num2.to_string());
-            sub_res.push(num3.to_string());
-            sub_res.push(num4.to_string());
-            sub_res.push(num5.to_string());
-            sub_res.push(num6.to_string());
-            res.push(sub_res.clone());
-        }
-    }
-    res
-}
-
 fn setup_data_knn(bt: &mut Template, dim: usize) -> Vec<String> {
     let mut res = Vec::new();
     let mut rng = rand::thread_rng();
